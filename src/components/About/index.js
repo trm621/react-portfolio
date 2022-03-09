4a5ba154c458bf84207336f95cc84125e91be740
trm621 <t.maher621@gmail.com>
2022-03-09 13:14:12 -0500

    all photos appearing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import ProfilePic from '../../assets/profile-picture.jpg'
 
 function About() {
     return (
       <section className="about my-5">
-        <h1 id="about">About Me</h1>
-        <img src="./assets/profile-picture.jpg"/>
+        <h1 >About Me</h1>
+        <div id="about">
+        <img className="profile-pic" src={ProfilePic} />
         <div className="my-2">
         <p>
             How are you today? My name is Thomas Maher. My friends call me Tom. I'm a full stack web developer operating out of Norwalk, CT. I love gaming, watching scary movies, playing Dungeons and Dragons, and making people laugh. See my
             work in the projects section!
         </p>
+        </div>
         </div>
       </section>
     );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,45 +1,71 @@
 import React, { useState } from "react";
-import Modal from "../Modal";
 
 function Project({ category }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
     const [currentProject, setCurrentProject] = useState();
-    // TO DO: Clean up tech used and project descriptions
     const [projects] = useState([
+      {
+        name: "Trail-Talk",
+        description: "A social media platfrom to share experiences in national parks with others from around the world!",
+        imgfile: require('../../assets/project-image-4.jpg'),
+        link: 'https://pacific-shelf-58337.herokuapp.com/',
+        github: 'https://github.com/trm621/uconn-project-two',
+      },
       {
         name: "Beach-Blog",
         description: "A blog to post about your favorite beaches!",
         imgfile: require('../../assets/main-project-image.jpg'),
         link: 'https://evening-retreat-41294.herokuapp.com/',
         github: 'https://github.com/trm621/uconn-project-two',
+      },
+      {
+        name: "Binge-Buddy",
+        description: "A website built to help find the perfect dinner/movie pairing for movie night!",
+        imgfile: require('../../assets/project-image-1.jpg'),
+        link: 'https://trm621.github.io/movie-night-generator/',
+        github: 'https://github.com/trm621/movie-night-generator',
+      },
+      {
+        name: "Password-Generator",
+        description: "A simple application to generate a strong password.",
+        imgfile: require('../../assets/project-image-5.png'),
+        link: 'https://trm621.github.io/password-generator/',
+        github: 'https://github.com/trm621/password-generator',
+      },
+      {
+        name: "Bugdet-Tracker",
+        description: "An application to help manage your budget and spending",
+        imgfile: require('../../assets/project-image-7.png'),
+        link: 'https://fathomless-reaches-87217.herokuapp.com/',
+        github: 'https://github.com/trm621/budget-tracker',
+      },
+      {
+        name: "ReadMe-Generator",
+        description: "A node application used to generate a professional ReadMe file.",
+        imgfile: require('../../assets/project-image-6.jpg'),
+        link: 'https://github.com/trm621/readme-buddy',
+        github: 'https://github.com/trm621/readme-buddy',
       }
     ]);
   
     const currentProjects = projects.filter(
       (project) => project.category === category
     );
-  
-    const toggleModal = (image, i) => {
-      setCurrentProject({ ...image, index: i });
-      setIsModalOpen(!isModalOpen);
-    };
+
   
     return (
-      <div className="project">
-        {isModalOpen && (
-          <Modal onClose={toggleModal} currentProject={currentProject} />
-        )}
-        <div >
-          {currentProjects.map((image, i) => (
-            <img className="project-image"
-              src={image.imgfile}
-              alt={image.name}
-              onClick={() => toggleModal(image, i)}
-              key={image.name}
-            />
-          ))}
-        </div>
+      <div className="project-container">
+      <div className="card">
+        {currentProjects.map((image, i) => (
+          <img
+            src={image.imgfile}
+            alt={image.name}
+            className="img-thumbnail project"
+            
+          />
+        ))}
+        
       </div>
+    </div>
     );
   };
   
