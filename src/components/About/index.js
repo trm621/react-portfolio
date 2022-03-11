b15a65277774af30db9eabd179d9a3ae401bf974
trm621 <t.maher621@gmail.com>
2022-03-10 23:58:51 -0500

    added more to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,8 +14,8 @@ function About() {
         <p className="blurb">
             How are you today? My name is Thomas Maher. My friends call me Tom. I'm a full stack web developer operating out of Norwalk, CT. 
             I love gaming, watching scary movies, playing Dungeons and Dragons, and making people laugh. I recently earned a certificate 
-            through the UConn Coding Bootcamp, in which I learned how to implement new technologies on the fly while collaborating and building fun projects.
-            Through my experience, I gained valuable skills as a MERN stack developer.
+            through the UConn Coding Bootcamp, in which I learned how to implement new technologies on the fly while collaborating with fellow students and building fun projects.
+            Through my experience, I gained valuable skills as a MERN stack developer and earned so much insight into the amazing collaborative world of computer programming.
              See my work in the projects section!
         </p>
         </div>
